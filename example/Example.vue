baf495c1c1e567d44e0984656f4e9b2911868304
stackjie <stackjie@gmail.com>
2017-11-18 11:46:20 +0800

    init

---
+++ example/Example.vue
@@ -0,0 +1,251 @@
+<template lang="html">
+  <div id="example">
+    <ul class="switch-list">
+      <li class="switch-item" v-for="item in propList">
+        <span>{{ item.name }}: </span>
+        <zk-switch v-model="props[item.name]"></zk-switch>
+      </li>
+    </ul>
+    <zk-table
+      ref="table"
+      sum-text="sum"
+      index-text="#"
+      :data="data"
+      :columns="columns"
+      :stripe="props.stripe"
+      :border="props.border"
+      :show-header="props.showHeader"
+      :show-summary="props.showSummary"
+      :show-row-hover="props.showRowHover"
+      :show-index="props.showIndex"
+      :tree-type="props.treeType"
+      :is-fold="props.isFold"
+      :expand-type="props.expandType"
+      :selection-type="props.selectionType">
+      <template slot="$expand" scope="scope">
+        {{ `My name is ${scope.row.name},
+           this rowIndex is ${scope.rowIndex}.`
+         }}
+      </template>
+      <template slot="likes" scope="scope">
+        {{ scope.row.likes.join(',') }}
+      </template>
+    </zk-table>
+  </div>
+</template>
+
+<script>
+  import ZkSwitch from './Switch/Switch';
+
+  export default {
+    name: 'example',
+    components: {
+      ZkSwitch,
+    },
+    data() {
+      return {
+        props: {
+          stripe: false,
+          border: false,
+          showHeader: true,
+          showSummary: false,
+          showRowHover: true,
+          showIndex: false,
+          treeType: true,
+          isFold: true,
+          expandType: false,
+          selectionType: false,
+        },
+        data: [
+          {
+            name: 'Jack',
+            sex: 'male',
+            likes: ['football', 'basketball'],
+            score: 10,
+            children: [
+              {
+                name: 'Ashley',
+                sex: 'female',
+                likes: ['football', 'basketball'],
+                score: 20,
+                children: [
+                  {
+                    name: 'Ashley',
+                    sex: 'female',
+                    likes: ['football', 'basketball'],
+                    score: 20,
+                  },
+                  {
+                    name: 'Taki',
+                    sex: 'male',
+                    likes: ['football', 'basketball'],
+                    score: 10,
+                    children: [
+                      {
+                        name: 'Ashley',
+                        sex: 'female',
+                        likes: ['football', 'basketball'],
+                        score: 20,
+                      },
+                      {
+                        name: 'Taki',
+                        sex: 'male',
+                        likes: ['football', 'basketball'],
+                        score: 10,
+                        children: [
+                          {
+                            name: 'Ashley',
+                            sex: 'female',
+                            likes: ['football', 'basketball'],
+                            score: 20,
+                          },
+                          {
+                            name: 'Taki',
+                            sex: 'male',
+                            likes: ['football', 'basketball'],
+                            score: 10,
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                name: 'Taki',
+                sex: 'male',
+                likes: ['football', 'basketball'],
+                score: 10,
+              },
+            ],
+          },
+          {
+            name: 'Tom',
+            sex: 'male',
+            likes: ['football', 'basketball'],
+            score: 20,
+            children: [
+              {
+                name: 'Ashley',
+                sex: 'female',
+                likes: ['football', 'basketball'],
+                score: 20,
+                children: [
+                  {
+                    name: 'Ashley',
+                    sex: 'female',
+                    likes: ['football', 'basketball'],
+                    score: 20,
+                  },
+                  {
+                    name: 'Taki',
+                    sex: 'male',
+                    likes: ['football', 'basketball'],
+                    score: 10,
+                  },
+                ],
+              },
+              {
+                name: 'Taki',
+                sex: 'male',
+                likes: ['football', 'basketball'],
+                score: 10,
+                children: [
+                  {
+                    name: 'Ashley',
+                    sex: 'female',
+                    likes: ['football', 'basketball'],
+                    score: 20,
+                  },
+                  {
+                    name: 'Taki',
+                    sex: 'male',
+                    likes: ['football', 'basketball'],
+                    score: 10,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'Tom',
+            sex: 'male',
+            likes: ['football', 'basketball'],
+            score: 20,
+          },
+          {
+            name: 'Tom',
+            sex: 'male',
+            likes: ['football', 'basketball'],
+            score: 20,
+            children: [
+              {
+                name: 'Ashley',
+                sex: 'female',
+                likes: ['football', 'basketball'],
+                score: 20,
+              },
+              {
+                name: 'Taki',
+                sex: 'male',
+                likes: ['football', 'basketball'],
+                score: 10,
+              },
+            ],
+          },
+        ],
+        columns: [
+          {
+            label: 'name',
+            prop: 'name',
+            width: '400px',
+          },
+          {
+            label: 'sex',
+            prop: 'sex',
+            minWidth: '50px',
+          },
+          {
+            label: 'score',
+            prop: 'score',
+          },
+          {
+            label: 'likes',
+            prop: 'likes',
+            minWidth: '200px',
+            type: 'template',
+            template: 'likes',
+          },
+        ],
+      };
+    },
+    computed: {
+      propList() {
+        return Object.keys(this.props).map(item => ({
+          name: item,
+        }));
+      },
+    },
+    methods: {
+
+    },
+  };
+</script>
+
+<style scoped lang="less">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .switch-list {
+    margin: 20px 0;
+    list-style: none;
+    overflow: hidden;
+  }
+
+  .switch-item {
+    margin: 20px;
+    float: left;
+  }
+</style>
